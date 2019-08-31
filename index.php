8081ff7e2dcafb32eeaa436198ed4dd89e7cb054
Elian <cris.ufam2012@gmail.com>
2019-08-31 16:11:59 -0400

    Insercao de atributo à classe Pessoa

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 class Pessoa {
 	public $nome;
 	public $idade;
+	public $altura;
 
 	public function falarNome(){
 		echo $this->nome;
@@ -10,12 +11,17 @@ class Pessoa {
 	public function falarIdade(){
 		echo $this->idade;
 	}
+	public function falarAltura(){
+		echo $this->altura;
+	}
 }
 
 $uma_pessoa = new Pessoa;
 $uma_pessoa->nome = "Elian";
 $uma_pessoa->idade = "25";
+$uma_pessoa->altura = "1,75";
 
 $uma_pessoa->falarNome();
 $uma_pessoa->falarIdade();
+$uma_pessoa->falarAltura();
 ?>
